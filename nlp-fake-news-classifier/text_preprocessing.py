7ebd7dd51bc9227cda565fb2bced1f9c3cb10a38
Sebastian Cáceres Gelvez <sebastian.caceres.g@gmail.com>
2025-09-15 01:23:03 +0100

    Remove unused imports for ruff

--- nlp-fake-news-classifier/text_preprocessing.py
+++ nlp-fake-news-classifier/text_preprocessing.py
@@ -1,14 +1,14 @@
-import re
-import pandas as pd
-import numpy as np
+# import re
+# import pandas as pd
+# import numpy as np
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+# from nltk.stem import PorterStemmer
 
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
-# Download required NLTK data
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
+# # Download required NLTK data
+# nltk.download('punkt', quiet=True)
+# nltk.download('stopwords', quiet=True)
